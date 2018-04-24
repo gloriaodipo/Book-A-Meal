02c184f12b4a5957e7f5954f0d707fb82d19ddd0
gloriaodipo <gloriaodipo@gmail.com>
2018-04-24 17:57:46 +0300

    Chore:add app folder containing views.py

---
+++ app/views.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+class UserAPI(Resource):
+    def post(self):
+        pass
+
+    def get(self):
+        pass
+
+    
+class MealsAPI(Resource):
+    def post(self):
+        pass
+
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+class OrdersAPI(Resource):
+    def post(self):
+        pass
+
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    
+        
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
